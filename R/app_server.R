5f39f76a75b5bde0959f44937d43198e0bf357b6
Nathan <ncrane3d@gmail.com>
2025-06-25 15:00:18 -0600

    Initial contact page up

--- R/app_server.R
+++ R/app_server.R
@@ -6,4 +6,26 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
+
+  #About Me Images TODO: Replace with actual images
+  output$pfp_left<- renderImage( 
+    { 
+      list(src = "./resources/images/pfp_empty.png", height= "auto", width= "100%") 
+    }, 
+    deleteFile = FALSE
+  ) 
+
+  output$pfp_middle<- renderImage( 
+    { 
+      list(src = "./resources/images/pfp_empty.png", height= "auto", width= "100%") 
+    }, 
+    deleteFile = FALSE
+  ) 
+
+  output$pfp_right<- renderImage( 
+    { 
+      list(src = "./resources/images/pfp_empty.png", height= "auto", width= "100%") 
+    }, 
+    deleteFile = FALSE
+  ) 
 }
