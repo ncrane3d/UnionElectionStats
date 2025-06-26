f16b2dceb80e061d9454b18eb10a91d8e105087d
Lucy Lewark <144399771+inceptisol@users.noreply.github.com>
2025-06-26 10:03:54 -0600

    created bioPanel function

--- R/pages/about.R
+++ R/pages/about.R
@@ -1,34 +1,59 @@
 about <- function() {
+    loremIpsum <- "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat."
     nav_panel(
-    title = "About",
-    align = "right",
-    layout_columns(
-        card(
-            card_header("Research Purpose", align = "center"),
-            p("   Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu.
+        title = "About",
+        align = "center",
+        absolutePanel(
+            align = "center",
+            right = "10vw",
+            left = "10vw",
+            card(
+                card_header("Research Purpose", align = "center"),
+                p(
+                    "   Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu.
                 Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
                 Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
-                align = "left"
+                    align = "left"
+                )
+            ),
+            card(
+                height = "50vh",
+                card_header("Meet the Team", align = "center"),
+                div(
+                    align = "center"
+                ),
+                layout_column_wrap(
+                    width = 1 / 3,
+                    bioPanel(
+                        "First Last",
+                        "pfp_left",
+                        loremIpsum
+                    ),
+                    bioPanel(
+                        "First Last",
+                        "pfp_middle",
+                        loremIpsum
+                    ),
+                    bioPanel(
+                        "First Last",
+                        "pfp_right",
+                        loremIpsum
+                    ),
+                ),
             )
-        ),
-        card(
-            card_header("Meet the Team", align = "center"),
-            layout_columns(
-                imageOutput("pfp_left"),
-                imageOutput("pfp_middle"),
-                imageOutput("pfp_right"),
-                h2("First Last", align = "left"),
-                h2("First Last", align = "left"),
-                h2("First Last", align = "left"),
-                p("Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.", align = "left"),
-                p("Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.", align = "left"),
-                p("Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.", align = "left"),
-                col_widths = c(-1, 2, -2, 2, -2, 2, -1, -1, 2, -2, 2, -2, 2, -1, -1, 2, -2, 2, -2, 2, -1),
-                #row_heights = c(2, 1, 1)
-            )
-        ),
-        col_widths = c(-1, 10, -1, -1, 10),
-        page_fillable()
+            #page_fillable()
         )
+    )
+}
+
+bioPanel <- function(firstlast, img, description) {
+    absolutePanel(
+        div(),
+        align = "center",
+        width = "25vw",
+        height = "25vw",
+        imageOutput(img, width = "10vw", height = "10vw"),
+        h2(firstlast),
+        p(description)
     )
 }
