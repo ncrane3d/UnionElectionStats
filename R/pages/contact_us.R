368a9dc843f6742c86a18b3f0a269a0ffe39cde5
Lucy Lewark <144399771+inceptisol@users.noreply.github.com>
2025-06-26 15:13:25 -0600

    Inputs added

--- R/pages/contact_us.R
+++ R/pages/contact_us.R
@@ -1,9 +1,28 @@
 contact <- function() {
     nav_panel(
-    title = "Contact Us",
-    align = "right"
-    #In the example above, we used CSS to add a scaling background image to the page. You can also use img() to create this effect:
-    #Pass img() to any Shiny UI page method (e.g., page_fillable()). img() creates an image.
-    #Pass the path or URL of your desired image to img()’s src parameter. Set additional parameters to control the appearance of the image (e.g., width and height).
+        title = "Contact Us",
+        align = "center",
+        #In the example above, we used CSS to add a scaling background image to the page. You can also use img() to create this effect:
+        #Pass img() to any Shiny UI page method (e.g., page_fillable()). img() creates an image.
+        #Pass the path or URL of your desired image to img()’s src parameter. Set additional parameters to control the appearance of the image (e.g., width and height).
+        card(
+            card_header("Contact us"),
+            layout_column_wrap(
+                width = 1 / 3,
+                height = "5vh",
+                fill = TRUE,
+                textInput("name", "Name"),
+                textInput("email", "Email"),
+                textInput("subject", "Subject")
+            ),
+            textAreaInput(
+                "message",
+                "Message",
+                resize = "none",
+                value = "Your message here...",
+                width = "100%",
+            ),
+            submitButton(text = "Submit")
+        )
     )
 }
