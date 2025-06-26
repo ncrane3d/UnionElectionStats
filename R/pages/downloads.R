0e0e259573f073a490ca13e03eff60b3e44cf22c
Lucy Lewark <144399771+inceptisol@users.noreply.github.com>
2025-06-26 10:30:24 -0600

    Added cards

--- R/pages/downloads.R
+++ R/pages/downloads.R
@@ -1,6 +1,12 @@
 downloads <- function() {
     nav_panel(
-    title = "Downloads",
-    align = "right"
+        title = "Downloads",
+        align = "right",
+        layout_column_wrap(
+            width = 1 / 3,
+            card("test"),
+            card("test"),
+            card("test")
+        )
     )
 }
