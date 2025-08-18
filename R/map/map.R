5798d42e167a3e734d516a24d4fce35cebadf6f8
Lucy Lewark <144399771+inceptisol@users.noreply.github.com>
2025-08-18 11:56:55 -0600

    Layers mounted by proxy

--- R/map/map.R
+++ R/map/map.R
@@ -7,68 +7,9 @@ getPalette <- function(column) {
     colorNumeric(c("red", "blue"), column)
 }
 map <- function(input, output, pool, current_data_slice, current_query) {
-    boundaries <- getBoundaries(pool, current_query)
-    statePalette <- getPalette(boundaries[1]$state_count)
-    countyPalette <- getPalette(boundaries[2]$normalized_vote)
-    mapHighlight <- highlightOptions(
-        color = "white",
-        weight = 2,
-        opacity = 1,
-        bringToFront = TRUE
-    )
-    #Error handling for when there are no points to render on map
-    getCircleMarkerData <- function(){
-        if (nrow(current_data_slice()) > 1){
-            return(current_data_slice())
-        } else {
-            #Returns dataframe containing 1 point in Bangladesh, out of constrained view of user
-            return(data.frame(latitude=c(23.6850), longitude=c(90.3563), yrclosed=c(1), employer=c("none"), votes_for= c(1), votes_against=c(1)))
-        }
-    }
     return(renderLeaflet({
         leaflet(options = leafletOptions(minZoom = 3)) |>
             addTiles() |>
-            #State border layer
-            addPolygons(
-                data = boundaries[[1]](),
-                weight = 1,
-                color = ~ statePalette(state_count),
-                group = "states",
-                highlightOptions = mapHighlight
-            ) |>
-            #County border layer
-            addPolygons(
-                data = boundaries[[2]](),
-                weight = 1,
-                color = ~ countyPalette(normalized_vote),
-                group = "counties",
-            ) |>
-            #Individual election markers
-            addCircleMarkers(
-                data = getCircleMarkerData(),
-                color = "red",
-                stroke = FALSE,
-                fillOpacity = 0.75,
-                group = "counties",
-                clusterOptions = markerClusterOptions(
-                    #Expands overlapping markers out into a starburst on max zoom
-                    spiderfyOnMaxZoom = TRUE,
-                    showCoverageOnHover = FALSE
-                ),
-                #Popup on click of individual elections that displays basic info
-                label = ~ sprintf(
-                    "Employer: %s<br/>Year closed: %s<br/>Pro-union vote share: %s",
-                    employer,
-                    yrclosed,
-                    round(
-                        ((votes_for /
-                            (votes_for + votes_against)) *
-                            100),
-                        digits = 2
-                    )
-                ) %>%
-                    lapply(htmltools::HTML)
-            ) |>
             #Zoom based conditional rendering for layers
             groupOptions("counties", zoomLevels = 5:20) |>
             groupOptions("states", zoomLevels = 0:4) |>
@@ -79,12 +20,6 @@ map <- function(input, output, pool, current_data_slice, current_query) {
                 lat2 = 1,
                 lng2 = -54.892994
             ) |>
-            #Initial map location and zoom
-            setView(
-                lat = 39.82,
-                lng = -98.58,
-                zoom = 3,
-            ) %>%
             #On render, applies click event to visible polygons (using leaflet javascript library)
             onRender(
                 'function(el, x){
